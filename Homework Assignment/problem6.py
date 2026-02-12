a0334fbec97f9bb48db746477fe62588dd633bd8
arpitnegi29 <arpit.negi.cs28@iilm.edu>
2026-02-12 21:59:22 +0530

    Add rotate_by_one function to rotate array elements

---
+++ Homework Assignment/problem6.py
@@ -0,0 +1,22 @@
+def rotate_by_one(arr):
+    n = len(arr)
+    last_element = arr[n - 1]
+    for i in range(n - 1, 0, -1):
+        arr[i] = arr[i - 1]
+    arr[0] = last_element
+    return arr
+
+arr = [1, 2, 3, 4, 5]
+rotated_arr = rotate_by_one(arr)
+print("Rotated array:", rotated_arr)
+def rotate_by_one(arr):
+    n = len(arr)
+    last_element = arr[n - 1]
+    for i in range(n - 1, 0, -1):
+        arr[i] = arr[i - 1]
+    arr[0] = last_element
+    return arr
+
+arr = [1, 2, 3, 4, 5]
+rotated_arr = rotate_by_one(arr)
+print("Rotated array:", rotated_arr)
