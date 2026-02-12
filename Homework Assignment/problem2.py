2ebb61dbd06d0123e93162fc08371e25fca9c9a8
arpitnegi29 <arpit.negi.cs28@iilm.edu>
2026-02-12 21:50:25 +0530

    Create problem2.py

---
+++ Homework Assignment/problem2.py
@@ -0,0 +1,17 @@
+def find_min_max(arr):
+    if not arr:
+        return None
+    
+    min_val = arr[0]
+    max_val = arr[0]
+    
+    for num in arr[1:]:
+        if num < min_val:
+            min_val = num
+        elif num > max_val:
+            max_val = num
+    
+    return [min_val, max_val]
+
+arr = [1, 4, 3, 5, 8, 6]
+print(find_min_max(arr))
