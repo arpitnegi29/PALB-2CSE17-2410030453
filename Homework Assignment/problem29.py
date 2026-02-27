a74bbca78d80b1a1ffbea38c917a17b1b8f18e1e
arpitnegi29 <arpit.negi.cs28@iilm.edu>
2026-02-27 23:16:25 +0530

    Implement function to find median of a matrix

---
+++ Homework Assignment/problem29.py
@@ -0,0 +1,23 @@
+import bisect
+
+def matrixMedian(mat):
+    n = len(mat)
+    m = len(mat[0])
+
+    low = min(row[0] for row in mat)
+    high = max(row[-1] for row in mat)
+
+    desired = (n * m + 1) // 2
+
+    while low < high:
+        mid = (low + high) // 2
+        count = 0
+        for row in mat:
+            count += bisect.bisect_right(row, mid)
+
+        if count < desired:
+            low = mid + 1
+        else:
+            high = mid
+
+    return low
